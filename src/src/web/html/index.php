c159fc3c629f038d499a190503f143f4b1b4c023
applqpak <applqpak@gmail.com>
2016-02-18 20:37:30 +1100

    Update index.php

--- src/src/web/html/index.php
+++ src/src/web/html/index.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
 
+  <html>
+
+    <head>
+
+      <meta name="apple-mobile-web-capable" content="yes"
+
+      <meta name="viewport" content="width=device-width">
+
+    </head>
+
+    <body>
+
+      <strong>IC-iOS is still under development. Feel free to help out on <a href="https://github.com/ImagicalMine/IC-iOS/">GitHub</a></strong>
+
+    </body>
+
+  </html>
